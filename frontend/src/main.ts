4149fa1ac735b2885708143ff2f898aea1953894
Alan Graef <graefalan@gmail.com>
2020-02-19 17:59:53 -0300

    Adding vue-apollo

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,11 +3,13 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  apolloProvider: createProvider(),
+  render: (h) => h(App)
 }).$mount('#app');
