42718e3821cd7e356c1d4a969e86fd27a386e7ae
Julián Ortiz <or.julian.s@gmail.com>
2020-02-21 12:07:31 -0300

    Removing cached files in directory

--- backend/deps/guardian/lib/guardian/permissions/permission_encoding.ex
+++
@@ -1,8 +0,0 @@
-defmodule Guardian.Permissions.PermissionEncoding do
-  @moduledoc """
-  Behavior for the permission encoding
-  """
-
-  @callback encode(list() | integer(), String.t(), map()) :: term
-  @callback decode(list() | integer(), String.t(), map()) :: term
-end
