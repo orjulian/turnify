840b97dd298e4526c96ecaf099aed83ef168800c
Alan Graef <graefalan@gmail.com>
2020-02-21 00:05:26 -0300

    Basic signin and signup

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,32 @@
 <template>
   <div class="home">
+    <div>
+      <h3>Signin</h3>
+      Email
+      <input v-model="email">
+      Password
+      <input v-model="password">
+      <button @click="signIn()">Sign In</button>
+    </div>
+    <div>
+      <h3>Signup</h3>
+      Email
+      <input v-model="createEmail">
+      Password
+      <input v-model="createPassword">
+      Role ('patient' or 'admin' or 'profesional')
+      <input v-model="createRole">
+      <button @click="createUser()">Create</button>
+    </div>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <button @click="signIn()">Sign In</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-import { SIGNIN } from '@/graphql/mutations'
+import { SIGNIN, CREATE_USER } from '@/graphql/mutations'
 
 export default {
   name: 'Home',
@@ -18,11 +35,29 @@ export default {
   },
   data () {
     return {
-      email: '',
-      password: ''
+      email: 'alan@alan.com',
+      password: '123123',
+      createEmail: '',
+      createPassword: '',
+      createRole: 'patient'
     }
   },
   methods: {
+    createUser () {
+      this.$apollo.mutate({
+        mutation: CREATE_USER,
+        variables: {
+          email: this.createEmail,
+          password: this.createPassword,
+          passwordConfirmation: this.createPassword,
+          role: this.createRole
+        }
+      }).then( res => {
+        console.log(`YAY! created user ${res.data.createUser.email}`)
+      }).catch( err => {
+        console.log(`Ops! Problem creating user ${this.createEmail}`)
+      })
+    },
     signIn () {
       this.$apollo.mutate({
         mutation: SIGNIN,
@@ -31,7 +66,7 @@ export default {
           password: this.password
         }
       }).then( res => {
-        console.log(res)
+        console.log(res.data.signIn.token)
       }).catch( err => {
         console.log(err)
       })
